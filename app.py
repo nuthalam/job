9fe2f4c4e330814cf91da1cff5ad98eda10723d4
job <nuthalam@roehampton.ac.uk>
2023-07-29 23:51:05 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import requests
+from requests.auth import HTTPBasicAuth
+
+Key = HTTPBasicAuth('2e03f95f-cf38-4bf9-b8c3-4ce1aa1bce41', '')
+app = Flask('Flask')
+
+@app.route("/")
+def reed():
+    api = "https://www.reed.co.uk/api/1.0/search"
+    data = {
+        "locationName": "London",
+        "temp": "True"
+    }
+    response = requests.get(api, params=data, auth=Key)
+    return response.json()
