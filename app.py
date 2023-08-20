e5cc9f5cf61e19891c32078f6478fa4af9203eb1
job <nuthalam@roehampton.ac.uk>
2023-08-20 19:18:23 +0100

    changes made in the function name

--- app.py
+++ app.py
@@ -4,19 +4,20 @@ from requests.auth import HTTPBasicAuth  # Import 'HTTPBasicAuth' from 'requests
 
 app = Flask(__name__)  # Create a Flask application instance
 
-Key = HTTPBasicAuth('2e03f95f-cf38-4bf9-b8c3-4ce1aa1bce41', '')# Set up API authentication
-@app.route("/", methods=['GET', 'POST'])  # Define a route for the root URL
+API_KEY = HTTPBasicAuth('fe4c8fa7-c07c-422e-9573-4659750ab08b', '')  # Set up API authentication
 
-def reed():
+@app.route("/", methods=['GET', 'POST'])  # Define a route for the root URL
+def jobs():
     if request.method == 'POST':  # Check if the request method is POST
         location = request.form.get("location", "")  # Get the value of 'location' from the form
         keywords = request.form.get("keywords", "")  # Get the value of 'keywords' from the form
         full_time = request.form.get("fullTime", False)  # Get the value of 'fullTime' from the form
         recruitment_agency = request.form.get("postedByRecruitmentAgency", False)  # Get the value of 'postedByRecruitmentAgency' from the form
         employerId = request.form.get("employerId", "")  # Get the value of 'employerId' from the form
-    api = "https://www.reed.co.uk/api/1.0/search"  # Define the API URL
-    data = {
-        "locationName": location,
+
+        api = "https://www.reed.co.uk/api/1.0/search"  # Define the API URL
+        data = {
+            "locationName": location,
             "temp": "True",
             "keywords": keywords,
             "fullTime": full_time,
@@ -24,24 +25,25 @@ def reed():
             "distanceFromLocation": 1,
             "employerId": employerId
         }  # Create a dictionary with the parameters for the API request
-    try:
-        response = requests.get(api, params=data, auth=Key)# Send an API GET request
 
-        # Check if the API request was successful (HTTP status code 200)
-        if response.status_code == 200:# Check if the response status code is 200 (OK)
-            job_results = response.json()# Parse the JSON response
-            if "results" in job_results and job_results["results"]:
+        try:
+            response = requests.get(api, params=data, auth=API_KEY)  # Send an API GET request
+
+            if response.status_code == 200:  # Check if the response status code is 200 (OK)
+                job_results = response.json()  # Parse the JSON response
+                if "results" in job_results and job_results["results"]:
                     job_listing = job_results['results'][0]  # Get the first job listing from the results
                     title = f"Job Title: {job_listing['jobTitle']}"  # Create a job title string
                     return render_template('index.html', title=title)  # Render the template with the job title
-            else:
+                else:
                     return "No job listings found."  # Return a message if no job listings were found
-        else:
+            else:
                 error_message = f"Error occurred while fetching job results. Status code: {response.status_code}"
                 return render_template('index.html', error_message=error_message)
-    except requests.exceptions.RequestException as e:
+        except requests.exceptions.RequestException as e:
             error = f"An error occurred: {e}"
             return render_template('index.html', error=error)
     return render_template('index.html')  # Render the template for the initial GET request
-if __name__ == "_main_":
+
+if __name__ == "__main__":
     app.run()  # Run the Flask application if the script is executed directly
