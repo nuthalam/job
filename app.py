931f6026db0e8f7b463b68cb0b5076cfa298d584
job <nuthalam@roehampton.ac.uk>
2023-08-20 17:47:01 +0100

    get fulltime value

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def reed():
     if request.method == 'POST':  # Check if the request method is POST
         location = request.form.get("location", "")  # Get the value of 'location' from the form
         keywords = request.form.get("keywords", "")  # Get the value of 'keywords' from the form
+        full_time = request.form.get("fullTime", False)  # Get the value of 'fullTime' from the form
     api = "https://www.reed.co.uk/api/1.0/search"
     data = {
         "locationName": "London",
